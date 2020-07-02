8c1d0aed3d2bda78fe50c86397c930061697e840
Haya4Taka <mumon.mum@gmail.com>
2020-07-02 21:59:02 +0900

    secondInputOptionを選択したら、ユーザー入力があったように表示させる

--- lib/steps_components/text/TextStep.jsx
+++ lib/steps_components/text/TextStep.jsx
@@ -65,7 +65,7 @@ class TextStep extends Component {
 
     const showAvatar = user ? !hideUserAvatar : !hideBotAvatar;
 
-    const imageAltText = user ? "Your avatar" : `${botName}'s avatar`;
+    const imageAltText = user ? 'Your avatar' : `${botName}'s avatar`;
 
     return (
       <TextStepContainer className={`rsc-ts ${user ? 'rsc-ts-user' : 'rsc-ts-bot'}`} user={user}>
